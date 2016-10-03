37f32028a1f81d11e4c352a90a4bac69e79a61cc
rossmari <roma-bujenko@yandex.ru>
2016-10-03 15:41:46 +0700

    add sample name to separated pages

--- lib/chemical_separator.rb
+++ lib/chemical_separator.rb
@@ -13,19 +13,22 @@ class ChemicalSeparator
   def separate
     points = find_points
     points.each_with_index do |coordinates, index|
-      move_points(coordinates[:start], coordinates[:stop], index + 1)
+      move_points(coordinates[:start], coordinates[:stop], index + 1, coordinates[:sample_name])
     end
     document.write(file_full_path)
   end
 
-  def move_points(start, stop, index)
+  def move_points(start, stop, index, sample_name)
     new_worksheet = document.add_worksheet("New page #{index}")
     original_sheet = document.worksheets[0]
 
     new_worksheet.add_cell(0, 0, 'Wavelength, nm')
     new_worksheet.add_cell(0, 1, 'Abs')
+    new_worksheet.add_cell(0, 2, 'Sample')
+    new_worksheet.add_cell(1, 2, sample_name)
     new_worksheet.sheet_data[0][0].change_horizontal_alignment('center')
     new_worksheet.sheet_data[0][1].change_horizontal_alignment('center')
+    new_worksheet.sheet_data[0][2].change_horizontal_alignment('center')
     new_worksheet.change_row_height(0, 15)
     new_worksheet.change_column_width(0, 15)
 
@@ -47,16 +50,22 @@ class ChemicalSeparator
     points_entry = nil
     row_index = 0
     last_row = sheet.count
+    sample = nil
     sheet.each do |row|
       content = row ? row[0].value.to_s : ''
+      if content == 'Sample:'
+        sample = row[1].value.to_s
+      end
       if content == 'Data Points' && points_entry.nil?
         points_entry ||= {}
         points_entry[:start] = row_index + 2
       end
       if (row.nil? || last_row == row_index) && points_entry
         points_entry[:stop] = row_index - 1
+        points_entry[:sample_name] = sample
         result << points_entry
         log_points(points_entry)
+        sample = nil
         points_entry = nil
       end
       row_index += 1
